d26d7997c39e3e5ec8baeb8bc493e8de2a15d74a
Stephane Zafirov <szafirov@expedia.com>
2018-11-22 20:36:52 -0500

    Multiple pairs

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ import {Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis} from 'recharts';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css'
 
+const MaxSampleRate = 1000
+
 class App extends Component {
 
   constructor(props) {
     super(props)
+    this.data = []
     this.chart = {
       margins: {
         top: 10,
@@ -23,126 +26,156 @@ class App extends Component {
     this.state = {
       rounds: 1000,
       defense: 2,
-      data: [],
+      pairs: 10,
       chartData: [],
       tableData: [],
       currentRow: 0,
-      currentPage: undefined,
+      currentPair: 0,
+      currentPage: 1,
     }
-    this.columns = [{
-      Header: 'Round',
-      accessor: 'round',
-    }, {
-      Header: 'Level',
-      accessor: 'level',
-    }, {
-      Header: 'Index',
-      accessor: 'index',
-    }, {
-      Header: 'Bet Slot',
-      accessor: 'betSlot',
-    }, {
-      Header: 'Bet ($)',
-      accessor: 'bet',
-    }, {
-      Header: 'Out',
-      accessor: 'outcome',
-    }, {
-      Header: 'Match',
-      accessor: 'match',
-      Cell: row => (
-        <span style={{
-          color: row.value === 'L' ? '#ff2e00' : '#57d500',
-          transition: 'all .3s ease'
-        }}>{row.value}</span>
-      )
-    }, {
-      Header: 'Gain',
-      accessor: 'gain',
-    }, {
-      Header: 'Total',
-      accessor: 'total',
-    }, {
-      Header: 'Max',
-      accessor: 'max',
-    }].map(column => {
+    this.columns = [
+      {
+        Header: 'Round',
+        accessor: 'round',
+        sortable: false
+      }, {
+        Header: 'Level',
+        accessor: 'level',
+        sortable: false
+      }, {
+        Header: 'Index',
+        accessor: 'index',
+        sortable: false
+      }, {
+        Header: 'Bet Slot',
+        accessor: 'betSlot',
+        sortable: false
+      }, {
+        Header: 'Bet ($)',
+        accessor: 'bet',
+        sortable: false
+      }, {
+        Header: 'Out',
+        accessor: 'outcome',
+        sortable: false
+      }, {
+        Header: 'Match',
+        accessor: 'match',
+        Cell: row => (
+          <span style={{
+            color: row.value === 'L' ? '#ff2e00' : '#57d500',
+            transition: 'all .3s ease'
+          }}>{row.value}</span>
+        ),
+        sortable: false
+      }, {
+        Header: 'Gain',
+        accessor: 'gain',
+        sortable: false
+      }, {
+        Header: 'Total',
+        accessor: 'total',
+        sortable: false
+      }, {
+        Header: 'Max',
+        accessor: 'max',
+        sortable: false
+      }
+    ].map(column => {
       column.maxWidth = 75
       return column
     })
 
-    this.viewPageFor = currentRow => {
+    this.viewPageFor = (currentRow, currentPair) => {
       const computePage = row => Math.floor((row - 1) / 20)
       const currentPage = computePage(currentRow)
-      if (this.state.currentRow !== currentRow) {
-        // console.debug(currentRow, this.state.currentPage, currentPage)
+      if (this.state.currentRow !== currentRow || this.state.currentPair !== currentPair) {
+        // console.debug(currentRow, currentPair, currentPage)
         this.setState({
           currentRow,
           currentPage,
-          tableData: this.data.filter(row => computePage(row.round) === currentPage)
+          currentPair,
+          tableData: this.data.filter(row => computePage(row.round) === currentPage && row.pair === currentPair)
         })
       }
     }
   }
 
   start() {
-    const { defense } = this.state
-    const leader = new Player(defense)
-    const follower = new Player(defense, leader)
-    this.pair = new Pair(leader, follower)
-    this.data = []
+    const { defense, pairs } = this.state
+    this.pairs = [...Array(pairs).keys()].map(index => {
+      const leader = new Player(defense)
+      const follower = new Player(defense, leader)
+      return new Pair(index, leader, follower)
+    })
     this.run()
   }
 
   run = () => {
     const { rounds } = this.state
-    const maxSampleSize = 1000
-    const sampleRate = Math.max(1, Math.floor(rounds / maxSampleSize))
-    this.data = []
-    const chartData = []
-    let maxTotal = 0
-    while (this.pair.round < rounds) {
-      const row = this.pair.play()
-      if (this.pair.leader.level > 0 && this.pair.total >= maxTotal) {
-        this.pair.resetLevel()
+    let max = 0
+    let total = 0
+    const computeRow = (round, pair) => {
+      const row = pair.play()
+      row.round = round
+      row.pair = pair.index
+      total += pair.gain
+      if (pair.leader.level > 0 && total >= max) {
+        pair.resetLevel()
       } else {
-        this.pair.evolve()
+        pair.evolve()
       }
-      maxTotal = Math.max(maxTotal, this.pair.total)
-      row.max = maxTotal
-      this.data.push(row)
-      if (this.pair.round % sampleRate === 0) chartData.push(row)
+      row.total = total
+      row.max = max = Math.max(max, total)
+      return row
     }
+    this.data = [...Array(rounds).keys()].flatMap(round => this.pairs.map(pair => computeRow(round, pair)))
+    const sampleRate = Math.max(1, Math.floor(rounds / MaxSampleRate))
+    const chartData = this.data.filter(row => row.round % sampleRate === 0)
     this.setState({ chartData });
-    this.viewPageFor(1)
+    this.viewPageFor(1, 0)
   }
 
   render() {
-    const { defense, rounds, chartData, tableData, currentRow } = this.state
+    const { defense, pairs, rounds, chartData, tableData, currentPair, currentRow } = this.state
+    console.debug(pairs)
     return (
       <div className="app">
-        <p>
-          <label>
-            Rounds: <input type="number" min="0" max="10000" value={rounds} style={{ width: 100 }}
-                           onChange={e => this.setState({ rounds: parseInt(e.target.value, 10) })} />
-          </label>
-          <label>
-            Defense: <input type="number" min="2" max="10" value={defense} style={{ width: 50 }}
-                            onChange={e => this.setState({ defense: parseInt(e.target.value, 10) })} />
-          </label>
-          <button className="app-btn" onClick={() => this.start()}>Start</button>
-        </p>
         <div className="container">
-          <AreaChart width={800} height={800} data={chartData} margin={this.chart.margins}>
+          <div style={{ width: 600 }}>
+            <label>
+              Rounds: <input type="number" min="0" max="10000" value={rounds} style={{ width: 100 }}
+                             onChange={e => this.setState({ rounds: parseInt(e.target.value, 10) })} />
+            </label>
+            <label>
+              Defense: <input type="number" min="2" max="10" value={defense} style={{ width: 50 }}
+                              onChange={e => this.setState({ defense: parseInt(e.target.value, 10) })} />
+            </label>
+            <label>
+              Pairs: <input type="number" min="1" max="10" value={pairs} style={{ width: 50 }}
+                            onChange={e => this.setState({ pairs: parseInt(e.target.value, 10) })} />
+            </label>
+            <button className="app-btn" onClick={() => this.start()}>Start</button>
+          </div>
+          <div style={{ width: 600 }}>
+            <label>
+              View Pair: <select onChange={e => this.viewPageFor(this.state.currentRow, parseInt(e.target.value, 10))}>
+                {[...Array(this.state.pairs).keys()].map(p => <option key={p} value={p}>P{p + 1}</option>)}
+              </select>
+            </label>
+          </div>
+        </div>
+        <div className="container">
+          <AreaChart width={600} height={700} data={chartData} margin={this.chart.margins}>
             <XAxis dataKey="round"/>
             <YAxis/>
             <CartesianGrid strokeDasharray="3 3"/>
             <Tooltip
               isAnimationActive={false}
               content={<CustomTooltip
-              onActive={this.viewPageFor}
+              onActive={round => this.viewPageFor(round, currentPair)}
             />} />
-            <Area type='monotone' dataKey='total' stroke='#8884d8' fill='#8884d8'
-                  isAnimationActive={false}/>
+            <Area type='monotone' dataKey='total' stroke='#8884d8' fill='#8884d8' isAnimationActive={false} />
           </AreaChart>
           <ReactTable
             data={tableData}
@@ -161,6 +194,9 @@ class App extends Component {
   }
 }
 
+/**
+ * @return {null}
+ */
 function CustomTooltip(props) {
   const { active, label, onActive } = props
   if (active && label) onActive(label)
--- src/Pair.js
+++ src/Pair.js
@@ -1,24 +1,20 @@
 export default class Pair {
 
-  constructor(leader, follower) {
-    this.round = 0
-    this.total = 0
+  constructor(index, leader, follower) {
+    this.index = index
     this.leader = leader
     this.follower = follower
   }
 
   play() {
-    this.round += 1
     this.outcome = Math.random() > 0.5
     const leaderState = this.leader.play(this.outcome)
     const followerState = this.follower.play(this.outcome)
     this.gain = this.leader.gain + this.follower.gain
-    this.total += this.gain
     const formatGain = gain => (gain >= 0 ? '+' : '-') + Math.abs(gain)
     const formatIndex = state => state.index + (state.star ? '*' : '')
     const format = (a, b) => a + ':' + b
     return {
-      round: this.round,
       index: format(formatIndex(leaderState), formatIndex(followerState)),
       level: format(leaderState.level, followerState.level),
       betSlot: format(leaderState.betSlot ? 1 : 0, followerState.betSlot ? 1 : 0),
@@ -26,7 +22,6 @@ export default class Pair {
       outcome: this.outcome ? 1 : 0,
       match: this.leader.won ? 'W:L' : 'L:W',
       gain: formatGain(leaderState.gain) + formatGain(followerState.gain) + '=' + this.gain,
-      total: this.total,
     }
   }
 
